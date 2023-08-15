04a62ffd2396b9e7a803159e3817842bb6ef8f36
shahzaibkhan7788 <125466418+shahzaibkhan7788@users.noreply.github.com>
2023-08-15 16:16:07 +0500

    Add files via upload

---
+++ list and dictionary.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # APPEND....
+
+# In[4]:
+
+
+#Append is used to add elements in the list.....it is built in function...which is used to add items in 
+#at last of the list
+kamran=["mathemtics","chemistry",100,200,300,400]
+kamran.append("shahzaib")
+##try to remember that it always add an items at the last index
+
+
+# In[5]:
+
+
+kamran
+
+
+# In[10]:
+
+
+kamran[1:5]
+
+
+# In[6]:
+
+
+##indexing in the list....
+kamran[2]
+
+
+# In[7]:
+
+
+##Remember one more thing ..that if you want to take specific arrangemnt of an elements in the list ...
+##then just try this syntax
+kamran[:]
+
+
+# In[8]:
+
+
+kamran[1:]
+
+
+# In[9]:
+
+
+##if you want to take an elements form the mid...then just remember the index of each elements...
+kamran[1:5]
+
+# # Insertion
+
+# In[ ]:
+
+
+##indexing in a list ...always start from 0...
+##irt is used to insert the value in the list...
+##the syntax is little diffenet ....fist we give an index and then give vale of that index
+khan=["kmran","room",234,234,24,2,42,4,2233,42,4]
+khan.insert[1,"khalid"]
+
+
+# In[17]:
+
+
+khan
+
+
+# # Another inbuilt function........Extended method
+# ####always try to leasrn the defiantion by pressing shift + tab.....on the parentthesis
+# ##tthis is slightly difference from the appended function beause it does nto give us nested loop
+
+# In[ ]:
+
+
+lst=[1,2,3,4]
+
+
+# In[ ]:
+
+
+lst.extend([8,9])
+
+
+# # Various operation that we can perform in a list
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
