17220f17711347d97061034267fe8ea04e3e9ccc
shahzaibkhan7788 <125466418+shahzaibkhan7788@users.noreply.github.com>
2023-08-15 15:48:29 +0500

    Add files via upload

---
+++ Boolean variables  , dictionary ,tuples (1).py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # python data structure and Boolean
+
+# BOOLEAN
+# BOOLEAN AND LOGICAL OPERRATOR
+# LISTS
+# COMPARISON OPERATOR
+# DICTIONRIES
+# TUPLES
+# SETS
+# 
+
+# # Boolean variables
+
+# Boolean values are the two constants objects false and true
+# they are used to repestent true values (other values can also be false or true)
+# In numeric contexts (for example ,when used as the arguements to an arithemtic operator ) they behave like the integers
+# 0 and 1 respectively...
+
+# In[1]:
+
+
+True
+#when ever we are working on a boolean variables it means that our out is true or false ...its purpose is to show true or 
+#false values .It is mmostly used in conditions.....
+
+
+# In[2]:
+
+
+False
+
+
+# In[3]:
+
+
+print(True,False)
+
+
+# In[4]:
+
+
+print(True)
+
+
+# In[5]:
+
+
+print(False)
+
+
+# In[6]:
+
+
+type(True)
+
+
+# In[8]:
+
+
+type(False)
+
+
+# In[ ]:
+
+
+#there are a lot of inbuilt functions...it means that there are a lot of functions whihc is already created by the python
+#just like libraries.......
+
+
+# In[ ]:
+
+
+my_str="shahzaib"
+
+
+# In[ ]:
+
+
+#if i press dot operator after writing name of the string ..then there show a lot of the inbuilt  fucntions and then 
+#press tab...
+#it will show you a lot of inbuit functions....
+
+
+# In[ ]:
+
+
+my_str='kamran2342'
+
+
+# In[19]:
+
+
+my_str.isnumeric()
+
+
+# In[ ]:
+
+
+print(str.capitalize())
+print(str.casefold())
+print(str.center())
+print(str.count)
+print(str.encode)
+print(str.find())
+print(str.format())
+print(str.startswith(k))
+
+
+# # Booean and logical operator
+
+# In[20]:
+
+
+True and False
+
+
+# In[21]:
+
+
+True or False
+
+
+# In[23]:
+
+
+True and True
+
+
+# In[24]:
+
+
+str_example ='Hello world'
+my_str='Krish'
+str_example.isnumeric() or my_str.isalpha()
+
+
+# # LIST
+
+#  A  list is a data structure that is a changeable ,ordered sequence elements . Each element or value that is inside 
+#  of a list is called an item . Just as strings are defined as characters between qotes  ,list are definded by ahvin values between  square brackets....you can store strings digits but it should be seperated by commas
+
+# In[25]:
+
+
+type([])
+
+
+# In[30]:
+
+
+list_example=[]
+type(list_example)
+
+
+# In[33]:
+
+
+st=list()
+type(st)
+
+
+# In[35]:
+
+
+st=["shahzaib",2,3,4,4,5,5,"kamran",'Nouman']
+len(st)
+
+
+# In[36]:
+
+
+type(st)
+
+
+# # Append
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
